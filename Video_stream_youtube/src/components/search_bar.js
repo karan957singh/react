60bd5287e24c4b62e183ad2f235d35ecc849f006
rajkaran singh <karan957singh@gmail.com>
2018-06-21 21:54:49 +1000

    start of book_list

---
+++ Video_stream_youtube/src/components/search_bar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+class SearchBar extends Component{
+	constructor(props){
+		super(props);
+		this.state = {term:''};
+	}
+	render(){
+		// return <input onChange = { this.onInputChange } />;
+		// return <input onChange = {(event) => console.log(event.target.value) } />;
+		return (
+			<div className="search-bar">
+			<input
+			value={this.state.term}
+			 onChange = {event => this.onInputChange(event.target.value) } />
+			 <button className="btn btn-primary" 
+			 onClick={() => this.onInputChange()}>Search</button>
+			</div>
+			);
+	} 
+
+	onInputChange(term){
+		this.setState({term});
+		console.log(term);
+		this.props.onSearchTermChange(term);
+	}
+
+// 	onInputChange(event){ 
+// 	console.log(event);
+
+// }
+}
+
+// const SearchBar = () =>{
+  // return <input />;
+// }
+export default SearchBar;
