e39767e43e96be9944091c5fcf4484ffaf8457ad
rajkaran singh <karan957singh@gmail.com>
2018-06-21 21:39:12 +1000

    last commit for this app for now

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,27 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+class SearchBar extends Component{
+	constructor(props){
+		super(props);
+		this.state = {term:''};
+	}
+	render(){
+		// return <input onChange = { this.onInputChange } />;
+		// return <input onChange = {(event) => console.log(event.target.value) } />;
+		return (
+			<div className="search-bar">
+			<input
+			value={this.state.term}
+			 onChange = {event => this.onInputChange(event.target.value) } />
+			 <button className="btn btn-primary" 
+			 onClick={() => this.onInputChange()}>Search</button>
+			</div>
+			);
+	} 
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
+	onInputChange(term){
+		this.setState({term});
+		console.log(term);
+		this.props.onSearchTermChange(term);
+	}
+// 	onInputChange(event){ 
+// 	console.log(event);
 
-    this.state = { term: "" };
-  }
-
-  render() {
-    return (
-      <div className="search-bar">
-        <input
-          value={this.state.term}
-          onChange={event => this.onInputChange(event.target.value)}
-        />
-      </div>
-    );
-  }
-
-  onInputChange(term) {
-    this.setState({ term });
-    this.props.onSearchTermChange(term);
-  }
+// }
 }
 
+// const SearchBar = () =>{
+  // return <input />;
+// }
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,46 @@
-import _ from "lodash";
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import YTSearch from "youtube-api-search";
-import SearchBar from "./components/search_bar";
-import VideoList from "./components/video_list";
-import VideoDetail from "./components/video_detail";
-const API_KEY = "AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss";
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const YOUTUBE_KEY = 'AIzaSyBRp9r7F2LE8159lfXFnFZsUba5cOLqIys';
 
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    };
+// this component should produce some HTML
+class App extends Component{
+	constructor(props){
 
-    this.videoSearch("surfboards");
-  }
-
-  videoSearch(term) {
-    YTSearch({ key: API_KEY, term: term }, videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-    });
-  }
-
-  render() {
-    const videoSearch = _.debounce(term => {
-      this.videoSearch(term);
-    }, 300);
-
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-          videos={this.state.videos}
-        />
-      </div>
-    );
-  }
+		super(props);
+		this.state = { 
+			videos: [],
+			selectedVideo:null
+		};
+		this.videoSearch('sci-fi');
+		
+	}
+	videoSearch(term){
+	YTSearch({key:YOUTUBE_KEY, term: term}, (data) => {
+			this.setState({
+				videos:data,
+				selectedVideo:data[0]
+			});
+	});		
+	}
+render(){
+	const videoSearch =_.debounce((term)=> {this.videoSearch(term)},800);
+return (
+	<div>
+  <SearchBar onSearchTermChange={videoSearch}/>
+  <VideoDetail  video={this.state.selectedVideo} />
+  <VideoList 
+  		onVideoSelect={selectedVideo => this.setState({selectedVideo})}	
+  		videos={this.state.videos} />
+  </div>
+  );	
+}
+  
 }
 
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDom.render(<App />, document.querySelector(".container"));
