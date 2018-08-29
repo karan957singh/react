69d3daccb8dc9f87274d0808b5303e335ac5baaf
rajkaran singh <karan957singh@gmail.com>
2018-08-29 21:20:12 +1000

    last commit

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxPromise from 'redux-promise';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(rootReducer)}>
+    <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
